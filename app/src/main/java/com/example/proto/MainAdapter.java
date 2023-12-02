7f7ef2335401a5134326629db0c4b9cc566d228d
Elias <eliascanuas@gmail.com>
2023-12-02 14:10:11 -0600

    con imagenes todo

--- app/src/main/java/com/example/proto/MainAdapter.java
+++ app/src/main/java/com/example/proto/MainAdapter.java
@@ -3,11 +3,13 @@ package com.example.proto;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
@@ -21,6 +23,12 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel, MainAdapter.
     protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull MainModel model) {
         holder.Dia.setText(model.getDia());
         holder.Tipo.setText(model.getTipo());
+
+        Glide.with(holder.imagen.getContext())
+                .load(model.getIMG())
+                .placeholder(com.google.firebase.database.R.drawable.common_google_signin_btn_icon_dark)
+                .error(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark_normal)
+                .into(holder.imagen);
     }
 
     @NonNull
@@ -32,12 +40,14 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel, MainAdapter.
 
     class  myViewHolder extends RecyclerView.ViewHolder{
         TextView Dia,Tipo;
+        ImageView imagen;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
 
             Dia = (TextView) itemView.findViewById(R.id.diatext);
             Tipo = (TextView) itemView.findViewById(R.id.tipotext);
+            imagen = itemView.findViewById(R.id.imagen);
         }
     }
 }
--- app/src/main/java/com/example/proto/MainModel.java
+++ app/src/main/java/com/example/proto/MainModel.java
@@ -2,14 +2,16 @@ package com.example.proto;
 
 public class MainModel {
 
-    String Dia,Tipo;
+    String Dia,Tipo, IMG;
 
     MainModel(){
 
     }
-    public MainModel(String dia, String tipo) {
+
+    public MainModel(String dia, String tipo, String IMG) {
         Dia = dia;
         Tipo = tipo;
+        this.IMG = IMG;
     }
 
     public String getDia() {
@@ -26,5 +28,13 @@ public class MainModel {
 
     public void setTipo(String tipo) {
         Tipo = tipo;
+    }
+
+    public String getIMG() {
+        return IMG;
+    }
+
+    public void setIMG(String IMG) {
+        this.IMG = IMG;
     }
 }
